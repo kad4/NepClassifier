aa06deebe26f8fb66388e137c022f53208b2e5fa
diwasblack <diwasblack@gmail.com>
2016-01-21 12:51:22 +0545

    Refactoring vectorizer.py

--- NepClassifier/vectorizer.py
+++ NepClassifier/vectorizer.py
@@ -1,5 +1,4 @@
 import os
-import logging
 
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
@@ -11,28 +10,25 @@ class TfidfVectorizer():
     """ Class to vectorizer the given document"""
 
     def __init__(self):
-        self.base_path = os.path.dirname(__file__)
-
-        # Filepath for dictionary
-        self.dict_path = os.path.join(self.base_path, 'tokens.dict')
-
-        self.dictionary = None
-        self.tf_idf_model = None
-
+        # Stemmer to use
         self.stemmer = NepStemmer()
 
-        self.load_data()
+        # Absolute path for dictionary file
+        dict_path = os.path.join(os.path.dirname(__file__), "tokens.dict")
 
-    def load_data(self):
-        if(not(os.path.exists(self.dict_path))):
-            raise Exception('Dictionary not found')
+        if(not(os.path.exists(dict_path))):
+            raise Exception('Dictionary file not found')
 
-        self.dictionary = Dictionary.load(self.dict_path)
+        # Load dictionary
+        self.dictionary = Dictionary.load(dict_path)
+        self.no_of_features = len(self.dictionary)
 
-        logging.debug('Initializing tfidf model')
+        # Construct the tf_idf_model
         self.tf_idf_model = TfidfModel(dictionary=self.dictionary)
 
     def doc2vector(self, text):
+        """ Returns the tf-idf vector for given document """
+
         tokens = self.stemmer.get_stems(text)
         text_bow = self.dictionary.doc2bow(tokens)
 
