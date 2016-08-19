61cb89fd62e566e7aa8c1f6a89ec764899e50809
Diwas Sharma <diwasblack@gmail.com>
2016-08-19 06:13:20 +0545

    Code refactor for tfidf

--- NepClassifier/__init__.py
+++ NepClassifier/__init__.py
@@ -2,4 +2,3 @@ from .stemmer import NepStemmer
 from .svm import SVMClassifier
 from .tfidf import TfidfVectorizer
 from .datasets import NeptextData, NeptextCorpus
-from .tokenizer import construct_dictionary, Tokenizer
--- NepClassifier/tfidf.py
+++ NepClassifier/tfidf.py
@@ -1,7 +1,10 @@
-from gensim.models import TfidfModel
-from gensim import matutils
+import os
 
-from .tokenizer import Tokenizer
+from gensim import matutils
+from gensim.models import TfidfModel
+from gensim.corpora import Dictionary
+
+from .stemmer import NepStemmer
 
 
 class TfidfVectorizer():
@@ -11,24 +14,64 @@ class TfidfVectorizer():
 
     def __init__(self):
 
-        # Initialize tokenizer
-        self.tokenizer = Tokenizer()
-        self.no_of_features = self.tokenizer.no_of_features
+        self.base_path = os.path.dirname(__file__)
 
-        # Construct the tf_idf_model
-        self.tf_idf_model = TfidfModel(dictionary=self.tokenizer.dictionary)
+        self.tokens_path = os.path.join(self.base_path, "tokens")
+
+        self.stemmer = None
+        self.dictionary = None
+        self.no_of_features = None
+        self.tf_idf_model = None
+
+    def construct_dictionary(self, corpus):
+        dictionary = Dictionary(corpus)
+        dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=1000)
+        dictionary.compactify()
+
+        dictionary.save(self.tokens_path)
+
+    def load_data(self):
+
+        if not self.dictionary:
+
+            if not os.path.exists(self.tokens_path):
+                raise Exception('Dictionary file not found')
+
+            self.stemmer = NepStemmer()
+            self.dictionary = Dictionary.load(self.tokens_path)
+            self.no_of_features = len(self.dictionary)
+
+            # Construct the tf_idf_model
+            self.tf_idf_model = TfidfModel(dictionary=self.dictionary)
 
     def doc2vector(self, document):
         """ Returns the sparse tf-idf vector for given document """
 
-        bag_of_words = self.tokenizer.obtain_bow(document)
+        tokens = self.stemmer.get_stems(document)
+        bag_of_words = self.dictionary.doc2bow(tokens)
 
         return (self.tf_idf_model[bag_of_words])
+
+    def obtain_feature_vector(self, document):
+        """
+        Returns a single dense tf-idf vector for a given document
+        """
+
+        self.load_data()
+
+        tf_idf_vector = matutils.sparse2full(
+            self.doc2vector(document),
+            self.no_of_features
+        ).reshape(1, -1)
+
+        return tf_idf_vector
 
     def obtain_feature_matrix(self, documents):
         """
         Returns the tf-idf dense matrix for the given documents
         """
+
+        self.load_data()
 
         input_matrix_sparse = [
             self.doc2vector(x)
@@ -41,15 +84,3 @@ class TfidfVectorizer():
         ).transpose()
 
         return input_matrix
-
-    def obtain_feature_vector(self, document):
-        """
-        Returns a single dense tf-idf vector for a given document
-        """
-
-        tf_idf_vector = matutils.sparse2full(
-            self.doc2vector(document),
-            self.no_of_features
-        ).reshape(1, -1)
-
-        return tf_idf_vector
