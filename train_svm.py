e8bcc23d5c40f40b8342d4b339a9c322975dd116
Aayush Subedi <subedi.aayush@gmail.com>
2016-09-12 17:25:53 +0545

    Added commandline args for selecting feature extractor

--- train_svm.py
+++ train_svm.py
@@ -1,12 +1,16 @@
-from NepClassifier import Word2VecVectorizer, NeptextData, SVMClassifier
-
+from NepClassifier import TfidfVectorizer, Word2VecVectorizer, NeptextData, SVMClassifier
+import sys
 
 if __name__ == "__main__":
     # Load data
     documents, labels = NeptextData.load_data()
 
     # Initialize vectorizer
-    vectorizer = Word2VecVectorizer()
+    vectorizer = None
+    if (len(sys.argv) > 1 and sys.argv[1] == "w2v"):
+        vectorizer = Word2VecVectorizer()
+    else:
+        vectorizer = TfidfVectorizer()
 
     clf = SVMClassifier(vectorizer)
     clf.train(documents, labels)
